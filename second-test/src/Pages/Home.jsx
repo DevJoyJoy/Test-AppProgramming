d1f052afe2b809e7a7cd6ad45392ddde3fd16a65
DevJoyJoy <joycenascimento2007@gmail.com>
2025-10-22 10:41:33 -0300

    Fixed some stuff but still need to do the gallery

--- second-test/src/Pages/Home.jsx
+++ second-test/src/Pages/Home.jsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 export function Home(){
-    const [users, setUsers] = useState({})
+    const [users, setUsers] = useState([])
     const navigate = useNavigate();
 
     const openUserPf = (user) => {
@@ -23,24 +23,31 @@ export function Home(){
     return(
         <>
             <div className='bg-purple-200 min-h-screen flex flex-col'>
-                <div className='bg-purple-950 w-full h-30 flex justify-between align-middle'>
+                <div className='bg-purple-950 w-full h-30 flex justify-center align-middle'>
                     <div className='text-white font-bold text-5xl'>Div/as Bosch</div>
                 </div>
                 <br />
 
-                <div className='flex justify-center align-middle'>
-                    <div className='bg-purple-300 flex w-450 h-auto flex-row rounded-2xl p-5'>
-                        <h1 className=''>Selecione o perfil</h1>
-                        <br />
-                        <ul>
-                            {/* o map não funciona de forma alguma e eu não achei o erro */}
-                            {users.map((user) => (
-                                <li key={user.id} onClick={() => openUserPf(user)} style={{cursor:"pointer"}}>
-                                    {user.id} - {user.nome}
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
+                <h1 className='m-5'>Selecione o perfil: </h1>
+                <div className='flex justify-between align-middle'>
+                    <ul>
+                            <div className='bg-purple-300 flex w-450 h-auto flex-row rounded-2xl p-5'>
+                                <br />
+                                <br />
+                                    {users.map((user) => (
+                                        <li key={user.id} onClick={() => openUserPf(user)} style={{cursor:"pointer"}}>
+                                            <br />
+                                            ID: {user.id}
+                                            <br />
+                                            Nome: {user.nome} {user.sobrenome}
+                                            <br />
+                                            Apelido: {user.apelido}
+                                            <br />
+                                            Status: {user.status}
+                                        </li>
+                                    ))}
+                            </div>
+                    </ul>
                 </div>
             </div>
         </>
